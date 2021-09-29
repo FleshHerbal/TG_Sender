fe037d51f880bda9eb25c591225b6068df598bd9
FleshHerbal <34520508+FleshHerbal@users.noreply.github.com>
2021-09-29 22:55:36 +0500

    Update BTT_Model.cs

--- TG_Sender/Models/BTT_Model.cs
+++ TG_Sender/Models/BTT_Model.cs
@@ -2,7 +2,7 @@
 {
     class BTT_Model
     {
-        public int balance { get; set; }
+        public long balance { get; set; }
         public int peers { get; set; }
         public int total_speed { get; set; }
         public int enabled_speed { get; set; }
